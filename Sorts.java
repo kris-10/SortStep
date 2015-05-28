42c872d2f5869ee5be773f8fb77d8154f949e508
kris-10 <kristenl_120@yahoo.com>
2015-05-28 09:12:26 -0700

    Update Sorts.java

--- Sorts.java
+++ Sorts.java
@@ -1,30 +1,74 @@
 import java.util.*;
 
-public class Sorts
-{
-  public void bubbleSort(int[] list)
-  {
-    //your code here
+public class Sorts{
+  public void bubbleSort(int[] list){
+    for(int outer=0; outer<list.length-1; outer++){
+      for(int inner=0; inner<list.length-outer-1; inner++){
+        if(list[inner]>list[inner+1]){
+          int temp=list[inner];
+          list[inner]=list[inner+1];
+          list[inner+1]=temp;
+        }
+      }
+    }
   }
 
-  public void selectionSort(int[] list)
-  {
-    //your code here
+  public void selectionSort(int[] list){
+    int temp, flag;
+    for (int outer=0; outer<list.length; outer++){
+      flag=outer;
+      for(int inner=outer+1; inner<list.length;inner++){
+        if(list[inner]<list[flag]){
+          flag=inner;
+        }
+      }
+      temp=list[outer];
+      list[outer]=list[flag];
+      list[flag]=temp;
+    }
   }
 
-  public void insertionSort(int[] list)
-  {
-    //your code here
+  public void insertionSort(int[] list){
+    for (int outer=1; outer<list.length;outer++){
+      int position=outer;
+      int key=list[position];
+      while (position>0 && list[position-1]>key){
+        list[position]=list[position-1];
+        position--;
+      }
+      list[position]=key;
+    }
   }
 
-  private void merge(int[] a, int first, int mid, int last)
-  {
-    //your code here
+  private void merge(int[] a, int first, int mid, int last){
+    int [] c=new int[a.length];
+    int nA=first;
+    int nB=mid+1;
+    for(int i=0; i<a.length; i++){
+      if(nA>mid){
+        nB++;
+      }
+      else if(nB>last){
+        nA++;
+      }
+      else if(a[nA]>a[nB]){
+        c[i]=a[nB];
+        nB++;
+      }else{
+        c[i]=a[nA];
+        nA++;
+      }
+    }
+    for(int i=0; i<c.length; i++){
+      a[i]=c[i];
+    }
   }
 
-  public void mergeSort(int[] a, int first, int last)
-  {
-    //your code here
+  public void mergeSort(int[] a, int first, int last){
+    if(first<last){
+      int mid=(first+last)/2;
+      mergeSort(a, first, mid);
+      mergeSort(a, mid+1, last);
+      merge(a, first,mid, last);
+    }
   }
-}
-
